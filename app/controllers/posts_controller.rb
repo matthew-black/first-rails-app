7ebc63149600e3ca502273dcae618c8d0ae34fd9
Matthew Black <matthewallenblack@gmail.com>
2017-10-29 22:59:11 -0500

    Add edit and delete. Eventually need to add login tho.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,6 +28,8 @@ class PostsController < ApplicationController
 
   def destroy
     @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
 
 root 'posts#index'
-resources :posts, only: [:index, :new, :show, :create, :destroy, :edit]
-# get '/posts' => 'posts#index'
-# get '/posts/new' => 'posts#new'
-# get '/posts/:id' => 'posts#show'
-# post '/posts' => 'posts#create'
+resources :posts
+
 
 end
