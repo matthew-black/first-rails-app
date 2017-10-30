d16020b2fae99e19c7136085e04525bebf1195c1
Matthew Black <matthewallenblack@gmail.com>
2017-10-29 20:19:20 -0500

    Just flailing now.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 root 'posts#index'
-resources :posts, only: [:index, :new, :show, :create]
+resources :posts, only: [:index, :new, :show, :create, :destroy]
 # get '/posts' => 'posts#index'
 # get '/posts/new' => 'posts#new'
 # get '/posts/:id' => 'posts#show'
