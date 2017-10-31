40748d13f18d480e6e9e1a20c0a0eb6915085f91
Matthew Black <matthewallenblack@gmail.com>
2017-10-30 23:14:19 -0500

    Change redirect location for logout.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,13 +9,13 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to '/login'
+      redirect_to '/'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login'
+    redirect_to '/'
   end
 
 end
